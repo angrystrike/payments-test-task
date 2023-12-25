6471568608d4aec31476af981712ad4077d0ad43
Maxim Melnik <melnik.max00@gmail.com>
2023-12-25 13:52:50 +0200

    Add CurrencyConverter helper

--- app/Helpers/CurrencyConverter.php
+++ app/Helpers/CurrencyConverter.php
@@ -2,11 +2,36 @@
 
 namespace App\Helpers;
  
-class CurrencyConverter {
+use App\Models\Currency;
 
-    public function test()
+
+class CurrencyConverter 
+{
+    /**
+     * Convert currency from one type to another.
+     *
+     * @param float  $inputValue      The input value to convert.
+     * @param string $inputCurrency   The input currency code.
+     * @param string $outputCurrency  The output currency code (default is 'usd').
+     *
+     * @return float  The converted currency value.
+     * @throws \InvalidArgumentException  If unsupported currencies are provided.
+     */
+    public function convert($inputValue, $inputCurrency, $outputCurrency = 'usd')
     {
-        return 345;
+        $inputCurrency = Currency::where('title', $inputCurrency)->first();
+        $outputCurrency = Currency::where('title', $outputCurrency)->first();
+
+        if (!$inputCurrency || !$outputCurrency) {
+            throw new \InvalidArgumentException('Unsupported currency');
+        }
+
+        // Convert the input value to USD using the rateToUsd of the input currency.
+        $usdValue = $inputValue / $inputCurrency->rateToUsd;
+        
+        // Convert the USD value to the output currency using the rateToUsd of the output currency.
+        $output = $usdValue * $outputCurrency->rateToUsd;
+
+        return $output;
     }
-    
 }
